f3aff268f14ec76792818b0d77f10331e6bb4e72
byh3071-cpu <byh3071@gmail.com>
2026-02-07 15:46:51 +0900

    Allow firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCSV10roV3mPluGBCONZwx47uULzC5sObE",
+    authDomain: "flexible-world.firebaseapp.com",
+    databaseURL: "httpsflexible-world-default-rtdb.firebaseio.com",
+    projectId: "flexible-world",
+    storageBucket: "flexible-world.firebasestorage.app",
+    messagingSenderId: "487090083126",
+    appId: "1487090083126web3dc40b5bff63e8d6140f97"
+};
